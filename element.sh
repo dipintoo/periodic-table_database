76a5d240af37023e0bd8683fc0f3969c10f89113
dipintoo <120252968+dipintoo@users.noreply.github.com>
2024-03-04 19:21:33 +0700

    Update element.sh

--- element.sh
+++ element.sh
@@ -2,6 +2,7 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
+# Fungsi seleksi input, apakah berupa simbol, nama ataupun nomor unsur kimia
 get_column() {
   SYMBOL="$1"
   LENGTH=$(echo -n "$SYMBOL" | wc -m)
@@ -17,12 +18,14 @@ get_column() {
   fi
 }
 
+# Fungsi untuk mencetak unsur kimia berikut informasi detailnya
 print_result() {
   while read -r BAR BAR NUMBER BAR SYMBOL BAR NAME BAR WEIGHT BAR MELTING BAR BOILING BAR TYPE; do
     echo "The element with atomic number $NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $WEIGHT amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
   done
 }
 
+# Fungsi untuk mengambil informasi unsur kimia dari database
 get_data() {
   COLUMN="$1"
   SYMBOL="$2"
@@ -39,12 +42,17 @@ get_data() {
   fi
 }
 
-# Main Script
+
+# MENJALANKAN SCRIPT
+
+# Input argumen (bisa berupa simbol, nama ataupun nomor unsur kimia)
 SYMBOL=$1
 
+# Jika input kosong, cetak pesan error
 if [[ -z $SYMBOL ]]; then
   echo "Please provide an element as an argument."
 else
+  # Ambil data yang sesuai dengan input dari database
   COLUMN=$(get_column "$SYMBOL")
   get_data "$COLUMN" "$SYMBOL"
 fi
